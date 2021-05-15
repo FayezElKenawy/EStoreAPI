169781eee73789896882ae4ac78956e8a6325022
Subhan Masimov <subhanmasimov02@gmail.com>
2021-05-15 07:05:12 +0400

    Validation added with AOP support

---
+++ Core/Utilities/Messages/AspectMessages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Messages
+{
+    public static class AspectMessages
+    {
+        public static string WrongValidationType = "Xətalı Doğrulama Tipi";
+        public static string WrongLoggerType = "Xətalı Loglama Tipi";
+    }
+}
